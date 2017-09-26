3f440598025238b7b2cc528a5f32448d24984824
Antoine McGrath <antoine.mcgrath@gmail.com>
2017-09-26 16:44:15 -0700

    Update wayback_refresh.py

--- wayback_refresh.py
+++ wayback_refresh.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python
 """
 
-# wayback_refresh.py  -  A python tool to check if a URL in the Wayback Machine is fresh :seedling: 🌱 if the most recent capture is stale the Wayback will refresh with a new capture of the URL.
+-  A python tool to refresh🌱 stale URLs in the Wayback Machine
 
-# wayback_refresh  -  
-A python tool to check if a URL in the Wayback Machine is fresh :seedling: 🌱, and recapture the URL if it is stale.
-
-Use the Internet Archive's ia_plugin to check for recent captures (with a user specified number of days) before creating a new capture request.
+Specify a URL & how fresh (# of days) you want the Wayback capture to be. 
+This script will use the Internet Archive's ia_plugin to update the capture if it is stale. 
 
 
 Usage:
 wayback_refresh.py [options] [URL]
 
 Options:
+  -d --days=<factor>        Set days still fresh🌱 [default: 365].
   -h --help                 Show this help message and exit.
-  -d --days=<factor>        Manually set days [default: 365].
-  -v --version              Show version.
+  -v --version              Show version.  
   -s --suppress             Suppress all responses. #ERROR Note: This does not work
-  -u --url_response         Allow fresh URL response only #ERROR Note: This does not work
+  -l --less_text            Allow fresh URL responses only. #ERROR Note: This does not work
   -a --author               Show author.     #ERROR Note: This does not work
   -thx --thanks             Show gratitude.  #ERROR Note: This does not work
-  -f   --fresh              print(chr(127793)) :seedling: 🌱 #ERROR Note: This does not work
 
 
-
-
-Run from bash with any of the following commands
---------
-
-### Run on a single URL(!):
+### Run on a single URL:
  *  `python3 wayback_refresh.py "URL"`
 
 ### Run on a single URL, do not recapture if fresher than: 7 days 
  *  `python3 -d 7 wayback_refresh.py "URL"`
 
-### For a list of URLs(!):
+### Run on a list of URLs:
  *  `cat "urls.txt" | while read -r line;  do python3 wayback_refresh.py $line; done`
-
-### (!)Default recapture is "Do not recapture if fresher than: 365 days" 
 
 
 
@@ -45,16 +35,18 @@ Functional notes:
 
     1. This script will instruct the Internet Archive's Wayback Machine to capture a URL if either:
        A. the URL has not been captured
-       B. the URL capture is stale (definition of fresh is specified by the user, by default fresh is within 365 days)
+       B. the URL capture is stale (by default fresh is within 365 days, to overide use -d #)
 
     2. This script does follow redirects.
        The final destination URL will be used to interact with the Wayback Machine
-       *Future versions may support suppresion of this feature.
+       *Future versions may support suppression of this feature.
 
-    3. If days is set to 0 via argument "-d 0" the URL will be submitted to the wayback.
-       Based on observation the wayback may reject resubmissions if recently recaptured
-       Perhaps the wayback has a limit on recaptures within a defined period
+    3. If days is set to 0 via argument "-d 0" the URL will be submitted to the Wayback.
 
+
+Comments, critiques? Contact me -> @AGreenDCBike (https://www.twitter.com/AGreenDCBike)  
+
+Like the Internet Archive as much as I do? Thank them for hosting the Wayback & much more! -> https://archive.org/donate/
 
 
 """
